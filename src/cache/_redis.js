a6cec481c74c9d5268411eb66ea986b4110660cd
wenchengshen <634284013@qq.com>
2020-06-24 08:41:57 +0800

    update:fead

--- src/cache/_redis.js
+++ src/cache/_redis.js
@@ -8,8 +8,6 @@ const  redis=require('redis')
 
 const {REDIS_CONF}=require('../conf/db')
 
-
-
 //创建客户端
 const redisClient=redis.createClient(REDIS_CONF.port,REDIS_CONF.host);
 redisClient.on('err',err=>{
